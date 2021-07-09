747c550b76e26acb3f3394f38162c1354f7ce858
akshayadvani <akshayadvani786@gmail.com>
2021-07-09 17:38:33 +0530

    Add files via upload

---
+++ Assignment 14 -Event/index.js
@@ -0,0 +1,15 @@
+const img1 = document.querySelector(".img1")
+const img2 = document.querySelector(".img2")
+const data_hide = document.querySelector(".data_hide")
+
+
+img1.addEventListener("mouseover", () => {
+    img1.src = "king2.jpg"
+})
+
+img1.addEventListener("mouseout", () => {
+    img1.src = "king.jpg"
+})
+
+
+
