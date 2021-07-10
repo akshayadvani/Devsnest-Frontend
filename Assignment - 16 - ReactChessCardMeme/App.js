13ba64c0900dc7413f44d95a81a280943d2265df
akshayadvani <akshayadvani786@gmail.com>
2021-07-10 18:47:54 +0530

    Update App.js

--- Assignment - 16 - ReactChessCardMeme/App.js
+++ Assignment - 16 - ReactChessCardMeme/App.js
@@ -1,11 +1,9 @@
 import './Apl.css';
-import HelloWorld from './Assignment15/HelloWorld';
 import Card from './Assignment16/Card';
 import Chess from './Assignment16/Chess';
 function App() {
   return (
     <div className="app">
-         <HelloWorld/>
          <Card />
          <Chess /> 
     </div>
